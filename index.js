ceca750dd4ff4136df753e5ab793c0971b66bf28
ShaneMit <shanemit@cox.net>
2020-09-25 18:00:17 -0700

    License points activity

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-let a = 'red'
-let b = 'blue'
+checkSpeed(130)
 
-let inverse = 
-
-console.log(a)
-console.log(b)
+function checkSpeed(speed) {
+  let speedLimit = 70
+  let kmPerPoint = 5
+  
+  if (speed < speedLimit + kmPerPoint) {
+    console.log('Ok')
+  } else { 
+    const points = Math.floor((speed - speedLimit) / kmPerPoint)
+    if (points >= 12){
+      console.log('License suspended')
+    } else {
+      console.log('Points', points)
+    }
+  }
+}
