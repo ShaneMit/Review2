db196bd5bf03da4373bab437143100e7d2a287dc
ShaneMit <shanemit@cox.net>
2020-11-13 15:40:37 -0800

    function rev

--- index.js
+++ index.js
@@ -1,48 +1,20 @@
-var userScore = ''
-var compScore = ''
-
-for (let i = 0; i < 10; i++) {
-  let choices = ['r', 'p', 's']
-  let userChoice = prompt('Pick r, p, or s')
-  let compChoice = choices[Math.floor(Math.random() * choices.length)]
-  console.log(userChoice)
-  console.log(compChoice)
-
-  if (userChoice === 'r' && compChoice === 's') {
-    userScore++
-    alert(`You win! User: ${userScore}`)
-  } else if (userChoice === 'r' && compChoice === 'p') {
-    compScore++
-    alert(`You lose. Comp: ${compScore}`)
-  } else if (userChoice === compChoice) {
-    alert(`Tie`)
-  } else if (userChoice === 'p' && compChoice === 'r') {
-    userScore++
-    alert(`You win! User: ${userScore}`)
-  } else if (userChoice === 'p' && compChoice === 's') {
-    compScore++
-    alert(`You lose. Comp: ${compScore}`)
-  } else if (userChoice === 's' && compChoice === 'p') {
-    userScore++
-    alert(`You win! User: ${userScore}`)
-  } else if (userChoice === 's' && compChoice === 'r') {
-    compScore++
-    alert(`You lose. Comp: ${compScore}`)
-  }
-
-  if (i === 9) {
-    if (userScore > compScore) {
-      alert(`Game over! YOU WIN!
-             You: ${userScore}
-             Comp: ${compScore}`)
-    } else if (compScore > userScore) {
-      alert(`Game over! YOU LOSE!
-             Comp: ${compScore}
-             You: ${userScore}`)
-    } else if (userScore === compScore) {
-      alert(`Game over! IT'S A TIE!
-             You: ${userScore}
-             Comp ${compScore}`)
-    }
-  }
+const adder = (a,b) => {
+ console.log(a + b)
 }
+
+const subtracter = (a,b) => {
+  console.log(a - b)
+}
+
+const multi = (a,b) => {
+  console.log(a * b)
+}
+
+const divide = (a,b) => {
+  console.log(a / b)
+}
+
+adder(2,5)
+subtracter(10,7) 
+multi(5,2)
+divide(15,5)
